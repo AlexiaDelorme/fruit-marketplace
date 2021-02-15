e33bdd5ba1139afdce72fd63041a31b304c07b04
Alexia Delorme <alexia.delorme@gmail.com>
2021-02-15 13:06:52 +0000

    Added user type for user default model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserType(models.Model):
+    USER_TYPE_CHOICES = [
+        (A, 'Farmer'),
+        (B, 'Client')
+    ]
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_type = models.CharField(max_length=20, choices=USER_TYPE_CHOICES)
