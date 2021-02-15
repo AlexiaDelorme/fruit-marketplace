16fb44540d4144fa475a7ad76482b05325964141
Alexia Delorme <alexia.delorme@gmail.com>
2021-02-15 13:54:53 +0000

    Created a custom user type model to extend user model

--- main/models.py
+++ main/models.py
@@ -1,10 +1,18 @@
+from django.db import models
 from django.contrib.auth.models import User
 
 
 class UserType(models.Model):
-    USER_TYPE_CHOICES = [
-        (A, 'Farmer'),
-        (B, 'Client')
+    """ Adding user type field to default user model """
+
+    USER_TYPES_CHOICES = [
+        ('A', 'Farmer'),
+        ('B', 'Client')
     ]
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    user_type = models.CharField(max_length=20, choices=USER_TYPE_CHOICES)
+    user_type = models.CharField(
+        max_length=20,
+        blank=False,
+        # default='A',
+        choices=USER_TYPES_CHOICES
+    )
